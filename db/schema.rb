96e66a500d390a8fe910e074213deffb9dc8b3b8
gitalek <sunny.os87@rambler.ru>
2018-10-27 14:41:47 +1000

    add null constraint to columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,43 +10,43 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_27_034621) do
+ActiveRecord::Schema.define(version: 2018_10_27_042227) do
 
   create_table "answers", force: :cascade do |t|
-    t.text "body"
-    t.boolean "correct"
-    t.integer "question_id"
+    t.text "body", null: false
+    t.boolean "correct", null: false
+    t.integer "question_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["question_id"], name: "index_answers_on_question_id"
   end
 
   create_table "categories", force: :cascade do |t|
-    t.string "title"
+    t.string "title", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "questions", force: :cascade do |t|
-    t.text "body"
-    t.integer "test_id"
+    t.text "body", null: false
+    t.integer "test_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["test_id"], name: "index_questions_on_test_id"
   end
 
   create_table "tests", force: :cascade do |t|
-    t.string "title"
-    t.integer "level"
-    t.integer "category_id"
+    t.string "title", null: false
+    t.integer "level", null: false
+    t.integer "category_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["category_id"], name: "index_tests_on_category_id"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "login"
+    t.string "name", null: false
+    t.string "login", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
