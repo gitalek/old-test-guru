c6a83f0945de7c65f498b70c0b25abfbd590cfb8
gitalek <sunny.os87@rambler.ru>
2018-10-29 15:39:11 +1000

    add class method to Test

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,3 +1,10 @@
 class Test < ApplicationRecord
   belongs_to :category
+
+  def self.select_by_category(category)
+    self.joins(:category)
+      .where('categories.title = ?', category)
+      .order(title: :desc)
+      .pluck(:title)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  def tests_by_level(level)
+
+  end
 end
